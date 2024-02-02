74d1de6b1c736aadc92a8e6774a66365dc995620
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-02 22:06:41 +0200

    Empty States

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
 
-function DashboardPage() {
-  return (
-    <div>Dashboard Root Page</div>
-  )
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import EmptyOrg from "./empty-org";
+import BoardList from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
 }
 
-export default DashboardPage
+function DashboardPage({ searchParams }: DashboardPageProps) {
+  const { organization } = useOrganization();
+  return (
+    <div className=" flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
+}
+
+export default DashboardPage;
