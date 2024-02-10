69ee29ff92dee47a07923c87c784bd2425efba4d
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-10 16:19:10 +0200

    Room Info

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -10,11 +10,9 @@ interface CanvasProps {
 }
 
 function Canvas({ boardId }: CanvasProps) {
-  const info = useSelf((me) => me.info);
-  console.log(info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
