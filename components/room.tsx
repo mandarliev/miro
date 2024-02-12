6501f7a6af99775cf822f3ed67f72326e11cfb74
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-12 16:29:41 +0200

    Insert Layer

--- components/room.tsx
+++ components/room.tsx
@@ -4,6 +4,8 @@ import { Component, ReactNode } from "react";
 import { RoomProvider } from "@/liveblocks.config";
 
 import { ClientSideSuspense } from "@liveblocks/react";
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+import { Layer } from "@/types/canvas";
 
 interface RoomProps {
   children: ReactNode;
@@ -13,7 +15,14 @@ interface RoomProps {
 
 export const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
-    <RoomProvider id={roomId} initialPresence={{ cursor: null }}>
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ cursor: null, selection: [] }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
+      }}
+    >
       <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>
